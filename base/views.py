9e78465af499843e14d0f54dbfcbd7a4d0a12c04
gshr <you@example.com>
2022-10-24 00:58:37 +0530

    first commit

---
+++ base/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import asyncio
+import requests
+import random
+import json
+from time import perf_counter
+from django.http import HttpResponse
+# Create your views here.
+
+
+
+class GetInfoView(APIView):
+
+    def get(self,request,id):
+        url =f'https://reqres.in/api/users/{id}'
+        result = requests.get(url)
+        return Response(json.loads(result.text))
+
+
